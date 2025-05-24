6b49b63ccda3047c0ac77f1019727258e587ce7c
Philippe Berto <philippe.berto@ispot.tv>
2025-05-24 09:52:20 -0300

    feat(tracer): add OpenTelemetry tracing implementation
    - Introduced a new package `tracer` to handle OpenTelemetry tracing.
    - Configurable tracing options via environment variables.
    - Implemented a function to initialize the tracer with an OTLP exporter.
    - Added resource attributes for service and application names.
    - Provided a method to create spans with a specified name and version.
    - Included a no-op shutdown function for disabled tracing.

---
+++ tracer.go
@@ -0,0 +1,85 @@
+package tracer
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/philippe-berto/logger"
+)
+
+const baseVersion = "1.0.0"
+
+type (
+	Config struct {
+		Enable   bool   `env:"TRACE_ENABLE" envDefault:"1"`
+		Endpoint string `env:"TRACE_ENDPOINT" envDefault:"simplest-collector.monitoring.svc.cluster.local:4318"`
+	}
+)
+
+func New(ctx context.Context, cfg Config, serviceName, appName string, log *logger.Logger) (func(context.Context) error, error) {
+	if !cfg.Enable {
+		return ShutdownNull, nil
+	}
+
+	exporter, err := otlptrace.New(ctx,
+		otlptracehttp.NewClient(
+			otlptracehttp.WithInsecure(),
+			otlptracehttp.WithEndpoint(cfg.Endpoint),
+		),
+	)
+	if err != nil {
+		log.WithFields(logger.Fields{"error": err.Error()}).
+			Error("Telemetry Tracer: Could not setup exporter")
+
+		return nil, err
+	}
+
+	resources, err := resource.New(ctx,
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("application", appName),
+		),
+	)
+	if err != nil {
+		log.WithFields(logger.Fields{"error": err.Error()}).
+			Error("Telemetry tracer: Could not setup resources")
+
+		return nil, err
+	}
+
+	tracer := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exporter)),
+		sdktrace.WithResource(resources),
+	)
+
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	otel.SetTracerProvider(tracer)
+
+	return exporter.Shutdown, nil
+}
+
+func BaseTracer(ctx context.Context, tracerName, tracerVersion, spanName string) (context.Context, trace.Span) {
+	if tracerVersion == "" {
+		tracerVersion = baseVersion
+	}
+
+	tracer := otel.GetTracerProvider().Tracer(
+		tracerName,
+		trace.WithInstrumentationVersion("semver:"+tracerVersion),
+	)
+
+	return tracer.Start(ctx, spanName)
+}
+
+func ShutdownNull(ctx context.Context) error {
+	return nil
+}
